b4a1f8af84621d9e776471e1e17060937c1e8fd2
jorgen_falk <jorgenfalk@users.noreply.github.com>
2007-12-18 20:24:24 +0000

    Created new repository package. Added two new repository implementations, one for InMem (static, compile time setup) and a code skeleton for a Hibernate version Modified TrackingId to implement Serializable (for Hibernate) and equals/hashCode.

--- dddsample/src/main/java/se/citerus/dddsample/domain/CargoRepositoryImpl.java
+++
@@ -1,7 +0,0 @@
-package se.citerus.dddsample.domain;
-
-public class CargoRepositoryImpl implements CargoRepository {
-  public Cargo find(TrackingId trackingId) {
-    return null;  //To change body of implemented methods use File | Settings | File Templates.
-  }
-}
--- dddsample/src/main/java/se/citerus/dddsample/domain/TrackingId.java
+++ dddsample/src/main/java/se/citerus/dddsample/domain/TrackingId.java
@@ -1,6 +1,32 @@
 package se.citerus.dddsample.domain;
 
-public class TrackingId {
-  public TrackingId(String s) {
-  }
+import java.io.Serializable;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+public class TrackingId implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private final String id;
+
+	public TrackingId(String id) {
+		this.id = id;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+	
+	
 }
