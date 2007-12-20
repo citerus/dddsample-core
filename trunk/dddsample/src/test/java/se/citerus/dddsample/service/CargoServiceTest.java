e1031b7c81c5d7e48414af39bad648c5722e63b3
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2007-12-20 09:59:24 +0000

    Introduced EasyMock and improved service layer test to verify transaction demarcation.
    Test suite now passes, let's keep it that way!

--- trunk/dddsample/src/test/java/se/citerus/dddsample/service/CargoServiceTest.java
+++ trunk/dddsample/src/test/java/se/citerus/dddsample/service/CargoServiceTest.java
@@ -1,38 +1,56 @@
 package se.citerus.dddsample.service;
 
-import org.aopalliance.aop.Advice;
-import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
+import static org.easymock.EasyMock.*;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
-import org.springframework.transaction.interceptor.TransactionAspectSupport;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import se.citerus.dddsample.domain.Cargo;
+import se.citerus.dddsample.domain.Location;
 import se.citerus.dddsample.domain.TrackingId;
-import se.citerus.dddsample.repository.CargoRepositoryInMem;
+import se.citerus.dddsample.repository.CargoRepository;
 
 
 public class CargoServiceTest extends AbstractDependencyInjectionSpringContextTests {
 
-  // TODO:
-  // find  nice way to test context configuration and
-  // transaction existence (and attributes),
-  // with repository layer stubbed out. Sketchy atm.
-
   CargoService cargoService;
-  CargoRepositoryInMem cargoRepository;
+  CargoRepository cargoRepository;
+  PlatformTransactionManager transactionManager;
 
   public CargoServiceTest() {
     setDependencyCheck(false);
   }
 
   public void setCargoService(CargoService cargoService) {
-    AspectJProxyFactory factory = new AspectJProxyFactory(cargoRepository);
-    factory.addAdvice(new TransactionVerifier());
-    this.cargoRepository = factory.getProxy();
     this.cargoService = cargoService;
   }
 
+  public void setCargoRepository(CargoRepository cargoRepository) {
+    this.cargoRepository = cargoRepository;
+  }
+
+  public void setTransactionManager(PlatformTransactionManager transactionManager) {
+    this.transactionManager = transactionManager;
+  }
+
+  @Override
+  protected void prepareTestInstance() throws Exception {
+    // TODO: verify an existing transaction inside the repository mock instead, this smells like we're testing framework code...
+    super.prepareTestInstance();
+    expect(cargoRepository.find(new TrackingId("XYZ"))).
+            andReturn(new Cargo(new TrackingId("XYZ"), new Location("ORIG"), new Location("DEST")));
+    replay(cargoRepository);
+
+    TransactionStatus ts = createMock(TransactionStatus.class);
+    replay(ts);
+    expect(transactionManager.getTransaction(isA(TransactionDefinition.class))).
+            andReturn(ts);
+    transactionManager.commit(isA(TransactionStatus.class));
+    replay(transactionManager);
+  }
+
   protected String[] getConfigLocations() {
-    return new String[] { "context-service.xml" };
+    return new String[] { "context-service.xml", "mock-context-persistence.xml" };
   }
 
   public void testCargoServiceFindByTrackingIdScenario() throws Exception {
@@ -41,13 +59,7 @@ public class CargoServiceTest extends AbstractDependencyInjectionSpringContextTe
     assertEquals(new TrackingId("XYZ"), cargo.trackingId());
   }
 
-  private static class TransactionVerifier implements Advice {
-    public Object invoke(MethodInvocation methodInvocation) throws Throwable {
-      if (TransactionAspectSupport.currentTransactionStatus() == null) {
-        fail("Transaction is manadatory");
-      }
-      return methodInvocation.proceed();
-    }
+  protected void onTearDown() throws Exception {
+    verify(cargoRepository, transactionManager);
   }
-
 }
