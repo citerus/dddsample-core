c1e480aa4e4a32f037f9c08d0e63fdae9d1cc0c9
jorgen_falk <jorgen_falk@c75de98b-4f3f-0410-889e-e06553617c56>
2008-02-01 08:44:48 +0000

    Initial import of HandlingEvent registration application

---
+++ trunk/register-app/src/main/java/se/citerus/registerapp/service/HandlingEventServiceWs.java
@@ -0,0 +1,17 @@
+package se.citerus.registerapp.service;
+
+import se.citerus.dddsample.ws.HandlingEventServiceEndpoint;
+
+
+public class HandlingEventServiceWs implements HandlingEventService {
+  private HandlingEventServiceEndpoint endPoint;
+  
+  public void register(String completionTime, String trackingId, String carrierMovementId, String unlocode, String eventType){
+    System.out.println(completionTime);
+    endPoint.register(completionTime, trackingId, carrierMovementId, unlocode, eventType);
+  }
+
+  public void setEndPoint(HandlingEventServiceEndpoint endPoint) {
+    this.endPoint = endPoint;
+  }
+}
