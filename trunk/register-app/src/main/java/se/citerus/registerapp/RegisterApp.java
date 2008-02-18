eca172f21ab7056c9857d60cc5dc19ed1752c63d
jorgen_falk <jorgen_falk@c75de98b-4f3f-0410-889e-e06553617c56>
2008-02-18 07:24:32 +0000

    Added first shot with gui validation. Note: need improvement...

--- trunk/register-app/src/main/java/se/citerus/registerapp/RegisterApp.java
+++ trunk/register-app/src/main/java/se/citerus/registerapp/RegisterApp.java
@@ -1,5 +1,8 @@
 package se.citerus.registerapp;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
@@ -11,12 +14,20 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.WindowConstants;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicBorders;
 import javax.swing.text.DefaultFormatter;
 
 import se.citerus.registerapp.service.HandlingEventService;
+import se.citerus.registerapp.validation.FormValidationDecorator;
+import se.citerus.registerapp.validation.FormValidationSwingDecorator;
+import se.citerus.registerapp.validation.MandatoryTextFieldValidator;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.debug.FormDebugPanel;
+import com.jgoodies.forms.factories.ButtonBarFactory;
 import com.jgoodies.forms.layout.FormLayout;
 
 public class RegisterApp {
@@ -33,13 +44,14 @@ public class RegisterApp {
   
   private HandlingEventService handlingEventService;
   private boolean debugUI;
+  private FormValidationDecorator validator;
 
 
 
   /////////////////////////////////////////////////////////////////////////////
   // GUI EVENT HANDLING
   /////////////////////////////////////////////////////////////////////////////
-  protected void onRegister() {
+  protected void onRegister(){
     assert handlingEventService != null : "No HandlingEventService available";
     
     handlingEventService.register(ISO8601_DATE_FORMAT.format(completionTimeField.getValue()), 
@@ -65,8 +77,13 @@ public class RegisterApp {
     initComponents();
     JComponent panel = createMainPanel();
     
+    
     frame = createFrame(panel);
     frame.pack();
+    centerOnScreen(frame);
+    
+    initValidation(frame);
+    
     frame.setVisible(true);
     
     clearForm();
@@ -91,6 +108,10 @@ public class RegisterApp {
     locationField.setText("");
     eventTypeField.setText("");
   }
+
+  public static Border getMandatoryBorder() {
+    return new CompoundBorder(new LineBorder(Color.RED), new BasicBorders.MarginBorder());
+  }
   
   private void initComponents(){
     completionTimeField = new JFormattedTextField(ISO8601_DATE_FORMAT);
@@ -102,11 +123,41 @@ public class RegisterApp {
     registerButton = new JButton("Register");
     registerButton.addActionListener(new ActionListener(){
       public void actionPerformed(ActionEvent e) {
-        onRegister();
+        if (validator.validate()){
+          onRegister();
+        }
       }
     });
   }
 
+  /**
+   * We need to do initialization of validators after the components and their parent frame has been initialized.
+   * @param frame 
+   * 
+   */
+  private void initValidation(JFrame frame){
+    validator = new FormValidationSwingDecorator(frame);
+    
+    validator.add(eventTypeField, new MandatoryTextFieldValidator("Event type can't be empty"));
+    validator.add(trackingIdField, new MandatoryTextFieldValidator("Tracking id can't be empty"));
+  }
+  
+  
+  /**
+   * Center a component on the screen.
+   * 
+   * @param component the component to be centered
+   */
+
+  public static void centerOnScreen(Component component) {
+      Dimension componentSize = component.getSize();
+      Dimension screenSize = component.getToolkit().getScreenSize();
+
+      component.setLocation(
+          (screenSize.width  - componentSize.width)  / 2,
+          (int) ((screenSize.height - componentSize.height) *0.45));
+
+  }
 
   private JFrame createFrame(JComponent panel) {
     JFrame f = new JFrame();
@@ -121,7 +172,7 @@ public class RegisterApp {
     JComponent fieldPanel = createFieldPanel();
     JComponent buttonPanel = createButtonPanel();
     
-    DefaultFormBuilder mainBuilder = createBuilder("right:pref");
+    DefaultFormBuilder mainBuilder = createBuilder("p");
     mainBuilder.append(fieldPanel);
     mainBuilder.append(buttonPanel);
     
@@ -130,16 +181,12 @@ public class RegisterApp {
 
 
   private JComponent createButtonPanel() {
-    DefaultFormBuilder builder = createBuilder("right:pref, 3dlu");
-    
-    builder.append(registerButton);
-    
-    return builder.getPanel();
+    return ButtonBarFactory.buildRightAlignedBar(registerButton);
   }
 
 
   private JComponent createFieldPanel() {
-    DefaultFormBuilder builder = createBuilder("right:pref, 3dlu, 100dlu, 3dlu, p");
+    DefaultFormBuilder builder = createBuilder("right:pref, 3dlu, 100dlu");
     
     builder.appendSeparator("Completion");
     builder.append("Completion Time", completionTimeField); builder.nextLine();
---
+++ trunk/register-app/src/main/java/se/citerus/registerapp/validation/FormValidationSwingDecorator.java
@@ -0,0 +1,114 @@
+package se.citerus.registerapp.validation;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Point;
+import java.util.HashMap;
+
+import javax.swing.ImageIcon;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class FormValidationSwingDecorator extends AbstractFormValidationDecorator{
+
+  private static final int POPUP_Y_OFFSET = 4;
+  private static final int POPUP_X_OFFSET = 12;
+  private Color oldColor = Color.WHITE;
+  private JFrame form;
+  private String message;
+  private HashMap<JComponent, JPanel> popups = new HashMap<JComponent, JPanel>();
+  private JPanel glassPane = new JPanel();
+
+  public FormValidationSwingDecorator(JFrame form) {
+    assert form != null : "Can't decorate null frame";
+    
+    this.form = form;
+    glassPane.setOpaque(false);
+    glassPane.setVisible(false);
+    glassPane.setLayout(null);
+//    glassPane.setPreferredSize(form.getPreferredSize());
+    glassPane.setSize(form.getSize());
+    form.setGlassPane(glassPane);
+  }
+
+  public FormValidationSwingDecorator(JFrame form, String message) {
+    this(form);
+    
+    this.message = message;
+  }
+
+  private JPanel createPopupRelativeTo(JComponent component) {
+    JPanel popupPane = null;
+    
+    if (popups.containsKey(component)){
+      popupPane = popups.get(component);
+    } else {
+      popupPane = new JPanel(new FlowLayout());
+      glassPane.add(popupPane);
+      
+      popupPane.add(getIconLabel());
+      popupPane.add(getMessageLabel());
+      popupPane.setLocation(getPopupPositionRelativeTo(component));
+      popupPane.setVisible(true);
+      popupPane.setOpaque(false);
+      popupPane.validate();
+      popupPane.setSize(popupPane.getPreferredSize());
+     
+    }
+    
+    return popupPane;
+  }
+
+  private Point getPopupPositionRelativeTo(JComponent component) {
+    Point compPos = component.getLocation();
+    Dimension compDim = component.getSize();
+    Point popupPos = new Point(compPos.x + POPUP_X_OFFSET, compPos.y + compDim.height + POPUP_Y_OFFSET);
+    
+    System.out.println("Comp pos: " + compPos + " - Popup pos: " + popupPos);
+    return popupPos;
+  }
+
+  private JLabel getMessageLabel() {
+    JLabel label = new JLabel(message != null ? message : "");
+    label.setOpaque(false);
+    return label;
+  }
+
+  private JLabel getIconLabel() {
+    JLabel label = new JLabel();
+    
+    java.net.URL imageURL = this.getClass().getResource("/icon/error_16x16.png");
+    if (imageURL != null){
+      ImageIcon icon = new ImageIcon(imageURL);
+      label.setIcon(icon);
+    } else {
+      System.out.println("No icon found");
+    }
+    
+    
+    label.setOpaque(false);
+    return label;
+  }
+  
+  @Override
+  public void decorate(JComponent component, ComponentValidator validator) {
+    this.message = validator.getMessage();
+    this.oldColor = component.getBackground();
+    component.setBackground(Color.PINK);
+    popups.put(component, createPopupRelativeTo(component));
+    glassPane.setVisible(true);
+  }
+
+  @Override
+  public void undecorate(JComponent component) {
+    component.setBackground(oldColor);
+    JPanel panel = popups.get(component);
+    if (panel != null){
+      panel.setVisible(false);
+    }
+    glassPane.setVisible(false);
+  }
+}
