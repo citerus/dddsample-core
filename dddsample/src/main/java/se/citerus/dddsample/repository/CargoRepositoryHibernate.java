835276ab6bf8a347564beff8c8b0a10ba7a9807c
peter_backlund <peterbacklund@users.noreply.github.com>
2008-03-17 13:23:56 +0000

    Improved comment on rationale behind the removed relation between DeliveryHistory and HandlingEvent.

--- dddsample/src/main/java/se/citerus/dddsample/repository/CargoRepositoryHibernate.java
+++ dddsample/src/main/java/se/citerus/dddsample/repository/CargoRepositoryHibernate.java
@@ -12,17 +12,18 @@ public class CargoRepositoryHibernate extends HibernateRepository implements Car
 
   HandlingEventRepository handlingEventRepository;
 
-  public Cargo find(TrackingId trackingId) {
+  public Cargo find(TrackingId tid) {
     Cargo cargo = (Cargo) getSession().
-            createQuery("from Cargo where trackingId = ?").
-            setParameter(0, trackingId).
+            createQuery("from Cargo where trackingId = :tid").
+            setParameter("tid", tid).
             uniqueResult();
-    /*
+    /*  There's no OR-mapped relation between the cargo delivery history and its handling events
+        because the handling events are in a different aggregate.
+
         If this extra database call were a problem, you might want to use a different model.
-        For example, calculate the effect/status of the cargo and store it separate from the
-        handling events.
-     */
-    cargo.deliveryHistory().addAllEvents(handlingEventRepository.findEventsForCargo(trackingId));
+        For example, you could calculate the effect/status of the cargo and store it separate from the
+        handling events. */
+    cargo.deliveryHistory().addAllEvents(handlingEventRepository.findEventsForCargo(tid));
     return cargo;
   }
 
