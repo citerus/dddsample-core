c7c7cc563d800362323f123aa4043f252989b549
patrikfr <patrikfr@c75de98b-4f3f-0410-889e-e06553617c56>
2008-04-18 12:46:38 +0000

    Fixed failing test.

--- trunk/dddsample/src/test/java/se/citerus/dddsample/service/CargoServiceTest.java
+++ trunk/dddsample/src/test/java/se/citerus/dddsample/service/CargoServiceTest.java
@@ -80,7 +80,7 @@ public class CargoServiceTest extends AbstractDependencyInjectionSpringContextTe
     Location finalDestination = new Location(new UnLocode("DE","STI"), "Destination");
     final Cargo cargo = new Cargo(new TrackingId("XYZ"), origin, finalDestination);
     Location sesto = new Location(new UnLocode("SE","STO"), "Stockholm");
-    HandlingEvent claimed = new HandlingEvent(cargo, new Date(10), new Date(20), HandlingEvent.Type.CLAIM, sesto);
+    HandlingEvent claimed = new HandlingEvent(cargo, new Date(10), new Date(20), HandlingEvent.Type.CLAIM, sesto, null);
     Location to = new Location(new UnLocode("MU","GER"), "München");
     CarrierMovement carrierMovement = new CarrierMovement(new CarrierMovementId("CAR_001"), sesto, to);
     HandlingEvent loaded = new HandlingEvent(cargo, new Date(12), new Date(25), HandlingEvent.Type.LOAD, sesto, carrierMovement);
