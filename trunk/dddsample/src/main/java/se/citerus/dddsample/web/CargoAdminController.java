342db70e2cb33047e5e800882aae54dc7cb5287e
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2008-06-15 18:39:31 +0000

    Introduced BookingServiceFacade as an application service, on top of the domain layer's BookingService and the repositories, as a DTO assembly point and remoting exposure layer (currently configured in context-remote.xml, but only used as local reference).
    CargoAdminController now sits on top on this facade. If time permits, the booking part of the application can be moved to another virtual machine at this point.

--- trunk/dddsample/src/main/java/se/citerus/dddsample/web/CargoAdminController.java
+++ trunk/dddsample/src/main/java/se/citerus/dddsample/web/CargoAdminController.java
@@ -1,21 +1,19 @@
 package se.citerus.dddsample.web;
 
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
-import se.citerus.dddsample.domain.*;
-import se.citerus.dddsample.repository.CarrierMovementRepository;
-import se.citerus.dddsample.repository.LocationRepository;
-import se.citerus.dddsample.service.BookingService;
-import se.citerus.dddsample.service.RoutingService;
+import se.citerus.dddsample.service.BookingServiceFacade;
 import se.citerus.dddsample.service.dto.CargoRoutingDTO;
 import se.citerus.dddsample.service.dto.ItineraryCandidateDTO;
 import se.citerus.dddsample.service.dto.LegDTO;
-import se.citerus.dddsample.service.dto.assembler.CargoRoutingDTOAssembler;
-import se.citerus.dddsample.service.dto.assembler.ItineraryCandidateDTOAssembler;
+import se.citerus.dddsample.service.dto.LocationDTO;
 import se.citerus.dddsample.web.command.RegistrationCommand;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Handles cargo routing and administration.
@@ -23,21 +21,16 @@ import java.util.*;
  */
 public final class CargoAdminController extends MultiActionController {
 
-  private BookingService bookingService;
-  private RoutingService routingService;
-  private LocationRepository locationRepository;
-  private CarrierMovementRepository carrierMovementRepository;
-
-  // DTO conversion is pushed out to above the service layer for the time being,
-  // pending a dedicated DTO remoting layer  
+  private BookingServiceFacade bookingServiceFacade;
 
   public Map registrationForm(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
     final Map<String, Object> map = new HashMap<String, Object>();
-    final List<UnLocode> unLocodes = bookingService.listShippingLocations();
+    final List<LocationDTO> dtoList = bookingServiceFacade.listShippingLocations();
+
     final List<String> unLocodeStrings = new ArrayList<String>();
 
-    for (UnLocode unLocode : unLocodes) {
-      unLocodeStrings.add(unLocode.idString());
+    for (LocationDTO dto : dtoList) {
+      unLocodeStrings.add(dto.getUnLocode());
     }
 
     map.put("unlocodes", unLocodeStrings);
@@ -47,58 +40,40 @@ public final class CargoAdminController extends MultiActionController {
   public void register(final HttpServletRequest request, final HttpServletResponse response,
                        final RegistrationCommand command) throws Exception {
 
-    final TrackingId trackingId = bookingService.registerNewCargo(
-      new UnLocode(command.getOriginUnlocode()),
-      new UnLocode(command.getDestinationUnlocode())
+    final String trackingId = bookingServiceFacade.registerNewCargo(
+      command.getOriginUnlocode(), command.getDestinationUnlocode()
     );
-    response.sendRedirect("show.html?trackingId=" + trackingId.idString());
+    response.sendRedirect("show.html?trackingId=" + trackingId);
   }
 
-  public Map list(HttpServletRequest request, HttpServletResponse response) {
+  public Map list(HttpServletRequest request, HttpServletResponse response) throws Exception {
     final Map<String, Object> map = new HashMap<String, Object>();
-    final List<Cargo> allCargos = bookingService.listAllCargos();
+    final List<CargoRoutingDTO> cargoList = bookingServiceFacade.listAllCargos();
 
-    final CargoRoutingDTOAssembler assembler = new CargoRoutingDTOAssembler();
-    final List<CargoRoutingDTO> dtoList = new ArrayList<CargoRoutingDTO>(allCargos.size());
-
-    for (Cargo cargo : allCargos) {
-      dtoList.add(assembler.toDTO(cargo));
-    }
-
-    map.put("cargoList", dtoList);
+    map.put("cargoList", cargoList);
     return map;
   }
 
-  public Map show(final HttpServletRequest request, final HttpServletResponse response) {
+  public Map show(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
     final Map<String, Object> map = new HashMap<String, Object>();
-    final TrackingId trackingId = new TrackingId(request.getParameter("trackingId"));
-    final Cargo cargo = bookingService.loadCargoForRouting(trackingId);
-    final CargoRoutingDTO dto = new CargoRoutingDTOAssembler().toDTO(cargo);
+    final String trackingId = request.getParameter("trackingId");
+    final CargoRoutingDTO dto = bookingServiceFacade.loadCargoForRouting(trackingId);
     map.put("cargo", dto);
     return map;
   }
 
-  public Map selectItinerary(final HttpServletRequest request, final HttpServletResponse response) {
+  public Map selectItinerary(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
     final Map<String, Object> map = new HashMap<String, Object>();
-    final TrackingId trackingId = new TrackingId(request.getParameter("trackingId"));
-
-    final Cargo cargo = bookingService.loadCargoForRouting(trackingId);
-    final RouteSpecification routeSpecification = RouteSpecification.forCargo(cargo, new Date());
-    final List<Itinerary> itineraries = routingService.requestPossibleRoutes(routeSpecification);
-
-    final List<ItineraryCandidateDTO> itineraryCandidates = new ArrayList<ItineraryCandidateDTO>(itineraries.size());
-    final ItineraryCandidateDTOAssembler dtoAssembler = new ItineraryCandidateDTOAssembler();
-    for (Itinerary itinerary : itineraries) {
-      itineraryCandidates.add(dtoAssembler.toDTO(itinerary));
-    }
+    final String trackingId = request.getParameter("trackingId");
+    final List<ItineraryCandidateDTO> itineraryCandidates = bookingServiceFacade.requestPossibleRoutesForCargo(trackingId);
 
     map.put("itineraryCandidates", itineraryCandidates);
-    map.put("trackingId", trackingId.idString());
+    map.put("trackingId", trackingId);
     return map;
   }
 
   public void assignItinerary(final HttpServletRequest request, final HttpServletResponse response) throws Exception {
-    final TrackingId trackingId = new TrackingId(request.getParameter("trackingId"));
+    final String trackingId = request.getParameter("trackingId");
 
     // TODO:  gah, stuck on indexoutofbounds (legs[0].fromUnlocode etc) when trying to bind...
     // Revisit and fix this with a proper command object, this is just hideous
@@ -112,26 +87,13 @@ public final class CargoAdminController extends MultiActionController {
     }
 
     final ItineraryCandidateDTO selectedItinerary = new ItineraryCandidateDTO(legDTOs);
-    final Itinerary itinerary = new ItineraryCandidateDTOAssembler().fromDTO(selectedItinerary, carrierMovementRepository, locationRepository);
 
-    bookingService.assignCargoToRoute(trackingId, itinerary);
+    bookingServiceFacade.assignCargoToRoute(trackingId, selectedItinerary);
 
     response.sendRedirect("list.html");
   }
 
-  public void setBookingService(final BookingService bookingService) {
-    this.bookingService = bookingService;
-  }
-
-  public void setRoutingService(final RoutingService routingService) {
-    this.routingService = routingService;
-  }
-
-  public void setLocationRepository(LocationRepository locationRepository) {
-    this.locationRepository = locationRepository;
-  }
-
-  public void setCarrierMovementRepository(CarrierMovementRepository carrierMovementRepository) {
-    this.carrierMovementRepository = carrierMovementRepository;
+  public void setBookingServiceFacade(BookingServiceFacade bookingServiceFacade) {
+    this.bookingServiceFacade = bookingServiceFacade;
   }
 }
