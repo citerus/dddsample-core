c13a3dad9dfb33f084f7fe0bc31fbbd65a895233
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2008-07-28 13:48:01 +0000

    Brought EventService closer to the domain by renaming it DomainEventNotifier and use ubiquitous language in the method signatures (fireHandlingEventRegistered is now cargoWasHandled) in order to make it naturally fit in among the domain services.

---
+++ trunk/dddsample/src/main/java/se/citerus/dddsample/application/messaging/ThreadBasedDomainEventNotifierImpl.java
@@ -0,0 +1,25 @@
+package se.citerus.dddsample.application.messaging;
+
+import se.citerus.dddsample.domain.model.handling.HandlingEvent;
+import se.citerus.dddsample.domain.service.DomainEventNotifier;
+import se.citerus.dddsample.domain.service.TrackingService;
+
+/**
+ * Thread based implementation.
+ */
+public class ThreadBasedDomainEventNotifierImpl implements DomainEventNotifier {
+
+  private TrackingService trackingService;
+
+  public void cargoWasHandled(final HandlingEvent event) {
+    new Thread(new Runnable() {
+      public void run() {
+        trackingService.notify(event.cargo().trackingId());
+      }
+    }).start();
+  }
+
+  public void setTrackingService(TrackingService trackingService) {
+    this.trackingService = trackingService;
+  }
+}
