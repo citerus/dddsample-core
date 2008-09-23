21028ffec1e57778af7dfa1bc38b8dd2291268d5
peter_backlund <peterbacklund@users.noreply.github.com>
2008-09-23 15:18:17 +0000

    [maven-release-plugin]  copy for tag register-app-1.0

--- dddsample/src/main/java/se/citerus/dddsample/domain/model/cargo/StatusCode.java
+++
@@ -1,8 +0,0 @@
-package se.citerus.dddsample.domain.model.cargo;
-
-/**
- * Represents the different status codes for a cargo.
- */
-public enum StatusCode {
-  NOT_RECEIVED, IN_PORT, ONBOARD_CARRIER, CLAIMED, UNKNOWN
-}
