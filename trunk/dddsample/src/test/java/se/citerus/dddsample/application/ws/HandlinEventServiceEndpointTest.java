69ebef1aba9899402a2d565ab9f34f60d8c5238a
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2008-09-29 19:51:44 +0000

    Refactored test to reflet introduction of handling event factory

--- trunk/dddsample/src/test/java/se/citerus/dddsample/application/ws/HandlinEventServiceEndpointTest.java
+++ trunk/dddsample/src/test/java/se/citerus/dddsample/application/ws/HandlinEventServiceEndpointTest.java
@@ -3,13 +3,17 @@ package se.citerus.dddsample.application.ws;
 import junit.framework.TestCase;
 import static org.easymock.EasyMock.*;
 import se.citerus.dddsample.application.service.InMemTransactionManager;
+import se.citerus.dddsample.domain.model.cargo.Cargo;
 import se.citerus.dddsample.domain.model.cargo.TrackingId;
+import se.citerus.dddsample.domain.model.carrier.CarrierMovement;
 import se.citerus.dddsample.domain.model.carrier.CarrierMovementId;
 import se.citerus.dddsample.domain.model.handling.HandlingEvent;
 import se.citerus.dddsample.domain.model.handling.HandlingEventFactory;
+import static se.citerus.dddsample.domain.model.location.SampleLocations.*;
 import se.citerus.dddsample.domain.model.location.UnLocode;
 import se.citerus.dddsample.domain.service.HandlingEventService;
 import se.citerus.dddsample.domain.service.UnknownCarrierMovementIdException;
+import se.citerus.dddsample.domain.service.UnknownLocationException;
 import se.citerus.dddsample.domain.service.UnknownTrackingIdException;
 
 import java.text.SimpleDateFormat;
@@ -19,80 +23,45 @@ public class HandlinEventServiceEndpointTest extends TestCase {
 
   private HandlingEventServiceEndpointImpl endpoint;
   private HandlingEventService handlingEventService;
-  private HandlingEventFactory handlingEventFactory;
   private SimpleDateFormat sdf;
+  private HandlingEvent event;
+  private Date date = new Date(100);
 
   protected void setUp() throws Exception {
-    handlingEventFactory = new HandlingEventFactory();
+    sdf = new SimpleDateFormat(HandlingEventServiceEndpointImpl.ISO_8601_FORMAT);
+
     endpoint = new HandlingEventServiceEndpointImpl();
+
     handlingEventService = createMock(HandlingEventService.class);
     endpoint.setHandlingEventService(handlingEventService);
-    sdf = new SimpleDateFormat(HandlingEventServiceEndpointImpl.ISO_8601_FORMAT);
+
     endpoint.setTransactionManager(new InMemTransactionManager());
+
+    Cargo cargo = new Cargo(new TrackingId("FOO"), HONGKONG, NEWYORK);
+    CarrierMovement carrierMovement = new CarrierMovement(
+      new CarrierMovementId("CAR_456"),
+      HONGKONG, TOKYO, new Date(), new Date());
+
+    event = new HandlingEvent(
+      cargo, date, new Date(), HandlingEvent.Type.LOAD, HONGKONG, carrierMovement
+    );
+
+    HandlingEventFactory handlingEventFactory = new HandlingEventFactory() {
+      public HandlingEvent createHandlingEvent(Date completionTime, TrackingId trackingId, CarrierMovementId carrierMovementId, UnLocode unlocode, HandlingEvent.Type type) throws UnknownTrackingIdException, UnknownCarrierMovementIdException, UnknownLocationException {
+        return event;
+      }
+    };
+
+    endpoint.setHandlingEventFactory(handlingEventFactory);
   }
 
   public void testRegisterValidEvent() throws Exception {
-    Date date = new Date(100);
-
-    final HandlingEvent event = handlingEventFactory.createHandlingEvent(date, new TrackingId("FOO"), new CarrierMovementId("CAR_456"), new UnLocode("CNHKG"), HandlingEvent.Type.LOAD);
     handlingEventService.register(event);
     replay(handlingEventService);
 
     // Tested call
     endpoint.register(sdf.format(date), "FOO", "CAR_456", "CNHKG", "LOAD");
   }
-
-
-  public void testRegisterUnknownTrackingId() throws Exception {
-    Date date = new Date(100);
-
-    TrackingId trackingId = new TrackingId("NOTFOUND");
-    UnLocode unlocode = new UnLocode("SESTO");
-
-    final HandlingEvent event = handlingEventFactory.createHandlingEvent(date, trackingId, null, unlocode, HandlingEvent.Type.CLAIM);
-    handlingEventService.register(event);
-    expectLastCall().andThrow(new UnknownTrackingIdException(trackingId));
-    replay(handlingEventService);
-
-    // Tested call
-    endpoint.register(sdf.format(date), "NOTFOUND", null, "SESTO", "CLAIM");
-  }
-
-  public void testRegisterUnknownCarrierMovementId() throws Exception {
-    Date date = new Date(100);
-
-    TrackingId trackingId = new TrackingId("XYZ");
-    CarrierMovementId carrierMovementId = new CarrierMovementId("NOTFOUND");
-
-    final HandlingEvent event = handlingEventFactory.createHandlingEvent(date, trackingId, carrierMovementId, new UnLocode("AUMEL"), HandlingEvent.Type.UNLOAD);
-    handlingEventService.register(event);
-    expectLastCall().andThrow(new UnknownCarrierMovementIdException(carrierMovementId));
-    replay(handlingEventService);
-
-    // Tested call
-    endpoint.register(sdf.format(date), "XYZ", "NOTFOUND", "AUMEL", "UNLOAD");
-  }
-
-  public void testRegisterInvalidEventType() throws Exception {
-    Date date = new Date(100);
-
-    replay(handlingEventService);
-
-    // Tested call
-    // Note: currently, every error is silently swallowed.
-    endpoint.register(sdf.format(date), "XYZ", "CAR_333", "AUMEL", "NO_SUCH_EVENT_TYPE");
-  }
-
-  public void testRegisterInvalidDateFormat() throws Exception {
-    Date date = new Date(100);
-
-    replay(handlingEventService);
-
-    // Tested call
-    // Note: currently, every error is silently swallowed.
-    endpoint.register("1 2 3 4", "XYZ", "CAR_333", "AUMEL", "LOAD");
-  }
-
 
   protected void tearDown() throws Exception {
     verify(handlingEventService);
