f0a00cdd821e4e94f0b30541d36523214c313fa4
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2008-12-14 19:44:24 +0000

    Using Voyage.Builder to create sample voyages.

--- trunk/dddsample/src/test/java/se/citerus/dddsample/domain/model/carrier/SampleVoyages.java
+++ trunk/dddsample/src/test/java/se/citerus/dddsample/domain/model/carrier/SampleVoyages.java
@@ -1,5 +1,6 @@
 package se.citerus.dddsample.domain.model.carrier;
 
+import static se.citerus.dddsample.DateTestUtil.toDate;
 import se.citerus.dddsample.domain.model.location.Location;
 import static se.citerus.dddsample.domain.model.location.SampleLocations.*;
 
@@ -24,6 +25,68 @@ public class SampleVoyages {
       new CarrierMovement(from, to, new Date(), new Date())
     )));
   }
+
+  // TODO CM00[1-6] and createVoyage are deprecated. Remove and refactor tests.
+
+  /**
+   * Voyage number 0100S (by ship)
+   *
+   * Hongkong - Hangzou - Tokyo - Melbourne - New York
+   */
+  public static final Voyage HONGKONG_TO_NEW_YORK =
+    new Voyage.Builder(new VoyageNumber("0100S"), HONGKONG).
+      addMovement(HANGZOU, toDate("2008-10-01", "12:00"), toDate("2008-10-03", "14:30")).
+      addMovement(TOKYO, toDate("2008-10-03", "21:00"), toDate("2008-10-06", "06:15")).
+      addMovement(MELBOURNE, toDate("2008-10-06", "11:00"), toDate("2008-10-12", "11:30")).
+      addMovement(NEWYORK, toDate("2008-10-14", "12:00"), toDate("2008-10-23", "23:10")).
+      build();
+
+
+  /**
+   * Voyage number 0200T (by train)
+   *
+   * New York - Chicago - Dallas
+   */
+  public static final Voyage NEW_YORK_TO_DALLAS =
+    new Voyage.Builder(new VoyageNumber("0200T"), NEWYORK).
+      addMovement(CHICAGO, toDate("2008-10-24", "07:00"), toDate("2008-10-24", "17:45")).
+      addMovement(DALLAS, toDate("2008-10-24", "21:25"), toDate("2008-10-25", "19:30")).
+      build();
+
+  /**
+   * Voyage number 0300A (by airplane)
+   *
+   * Dallas - Hamburg - Stockholm - Helsinki
+   */
+  public static final Voyage DALLAS_TO_HELSINKI =
+    new Voyage.Builder(new VoyageNumber("0300A"), DALLAS).
+      addMovement(HAMBURG, toDate("2008-10-29", "03:30"), toDate("2008-10-31", "14:00")).
+      addMovement(STOCKHOLM, toDate("2008-11-01", "15:20"), toDate("2008-11-01", "18:40")).
+      addMovement(HELSINKI, toDate("2008-11-02", "09:00"), toDate("2008-11-02", "11:15")).
+      build();
+
+  /**
+   * Voyage number 0301S (by ship)
+   *
+   * Dallas - Hamburg - Stockholm - Helsinki, alternate route
+   */
+  public static final Voyage DALLAS_TO_HELSINKI_ALT =
+    new Voyage.Builder(new VoyageNumber("0301S"), DALLAS).
+      addMovement(HELSINKI, toDate("2008-10-29", "03:30"), toDate("2008-11-05", "15:45")).
+      build();
+
+  /**
+   * Voyage number 0400S (by ship)
+   *
+   * Helsinki - Rotterdam - Shanghai - Hongkong
+   *
+   */
+  public static final Voyage HELSINKI_TO_HONGKONG =
+    new Voyage.Builder(new VoyageNumber("0400S"), HELSINKI).
+      addMovement(ROTTERDAM, toDate("2008-11-04", "05:50"), toDate("2008-11-06", "14:10")).
+      addMovement(SHANGHAI, toDate("2008-11-10", "21:45"), toDate("2008-11-22", "16:40")).
+      addMovement(HONGKONG, toDate("2008-11-24", "07:00"), toDate("2008-11-28", "13:37")).
+      build();
 
   public static final Map<VoyageNumber, Voyage> ALL = new HashMap();
 
