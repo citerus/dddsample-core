efaad98e340a2d07fe52b90d1468dd65e33d3f64
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2009-01-09 17:02:05 +0000

    TrackingService

--- trunk/dddsample/src/main/java/se/citerus/dddsample/application/impl/TrackingServiceImpl.java
+++ trunk/dddsample/src/main/java/se/citerus/dddsample/application/impl/TrackingServiceImpl.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang.Validate;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.transaction.annotation.Transactional;
-import se.citerus.dddsample.application.SystemEvents;
+import se.citerus.dddsample.application.ApplicationEvents;
 import se.citerus.dddsample.application.TrackingService;
 import se.citerus.dddsample.domain.model.cargo.Cargo;
 import se.citerus.dddsample.domain.model.cargo.CargoRepository;
@@ -12,18 +12,18 @@ import se.citerus.dddsample.domain.model.cargo.TrackingId;
 
 public class TrackingServiceImpl implements TrackingService {
 
-  private final SystemEvents systemEvents;
+  private final ApplicationEvents applicationEvents;
   private final CargoRepository cargoRepository;
   private final Log logger = LogFactory.getLog(getClass());
 
-  public TrackingServiceImpl(SystemEvents systemEvents, CargoRepository cargoRepository) {
-    this.systemEvents = systemEvents;
+  public TrackingServiceImpl(final ApplicationEvents applicationEvents, final CargoRepository cargoRepository) {
+    this.applicationEvents = applicationEvents;
     this.cargoRepository = cargoRepository;
   }
 
   @Override
   @Transactional(readOnly = true)
-  public void onCargoHandled(final TrackingId trackingId) {
+  public void inspectCargo(final TrackingId trackingId) {
     Validate.notNull(trackingId, "Tracking ID is required");
 
     final Cargo cargo = cargoRepository.find(trackingId);
@@ -35,11 +35,11 @@ public class TrackingServiceImpl implements TrackingService {
     // TODO cargo delivery status update would happen here
 
     if (cargo.isMisdirected()) {
-      systemEvents.cargoWasMisdirected(cargo);
+      applicationEvents.cargoWasMisdirected(cargo);
     }
 
     if (cargo.isUnloadedAtDestination()) {
-      systemEvents.cargoHasArrived(cargo);
+      applicationEvents.cargoHasArrived(cargo);
     }
   }
 
