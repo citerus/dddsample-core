36354338bb24fc93762dae1ebc64ec3b6e3360dd
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2009-01-24 16:32:52 +0000

    Added dates to TransitEdge, fixed RMI exposure of Pathfinder service

--- trunk/dddsample/src/main/java/com/partner/pathfinder/api/TransitEdge.java
+++ trunk/dddsample/src/main/java/com/partner/pathfinder/api/TransitEdge.java
@@ -1,31 +1,44 @@
 package com.partner.pathfinder.api;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
- * DTO for a leg in an itinerary.
+ * Represents an edge in a path through a graph,
+ * describing the route of a cargo.
+ *  
  */
 public final class TransitEdge implements Serializable {
 
-  private final String carrierMovementId;
+  private final String voyageNumber;
   private final String fromUnLocode;
   private final String toUnLocode;
+  private final Date fromDate;
+  private final Date toDate;
 
   /**
    * Constructor.
    *
-   * @param carrierMovementId
+   * @param voyageNumber
    * @param fromUnLocode
    * @param toUnLocode
+   * @param fromDate
+   * @param toDate
    */
-  public TransitEdge(final String carrierMovementId, final String fromUnLocode, final String toUnLocode) {
-    this.carrierMovementId = carrierMovementId;
+  public TransitEdge(final String voyageNumber,
+                     final String fromUnLocode,
+                     final String toUnLocode,
+                     final Date fromDate,
+                     final Date toDate) {
+    this.voyageNumber = voyageNumber;
     this.fromUnLocode = fromUnLocode;
     this.toUnLocode = toUnLocode;
+    this.fromDate = fromDate;
+    this.toDate = toDate;
   }
 
-  public String getCarrierMovementId() {
-    return carrierMovementId;
+  public String getVoyageNumber() {
+    return voyageNumber;
   }
 
   public String getFromUnLocode() {
@@ -36,4 +49,11 @@ public final class TransitEdge implements Serializable {
     return toUnLocode;
   }
 
+  public Date getFromDate() {
+    return fromDate;
+  }
+
+  public Date getToDate() {
+    return toDate;
+  }
 }
