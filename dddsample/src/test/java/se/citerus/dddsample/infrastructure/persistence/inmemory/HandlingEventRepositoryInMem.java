0ff1644d3ef981f735ffa5082deb0d3765db3a31
peter_backlund <peterbacklund@users.noreply.github.com>
2009-03-14 12:30:33 +0000

    Clean up inmem repositories

--- dddsample/src/test/java/se/citerus/dddsample/infrastructure/persistence/inmemory/HandlingEventRepositoryInMem.java
+++ dddsample/src/test/java/se/citerus/dddsample/infrastructure/persistence/inmemory/HandlingEventRepositoryInMem.java
@@ -1,109 +1,33 @@
 package se.citerus.dddsample.infrastructure.persistence.inmemory;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import se.citerus.dddsample.domain.model.cargo.Cargo;
 import se.citerus.dddsample.domain.model.cargo.TrackingId;
 import se.citerus.dddsample.domain.model.handling.HandlingEvent;
 import se.citerus.dddsample.domain.model.handling.HandlingEventRepository;
 import se.citerus.dddsample.domain.model.handling.HandlingHistory;
-import se.citerus.dddsample.domain.model.voyage.CarrierMovement;
-import se.citerus.dddsample.domain.model.voyage.VoyageRepository;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
+import java.util.*;
 
 public class HandlingEventRepositoryInMem implements HandlingEventRepository {
-  private final Log logger = LogFactory.getLog(getClass());
-  private final HashMap<String, HandlingEvent> eventDB = new HashMap<String, HandlingEvent>();
-  private VoyageRepository voyageRepository;
 
-  /**
-   * Initilaze the in mem repository.
-   * <p/>
-   * SpringIoC will call this init-method after the bean has bean created and properties has been set.
-   *
-   * @throws ParseException
-   */
-  public void init() throws ParseException {
-    /*
-    // CargoXYZ
-    final Cargo cargoXYZ = new Cargo(new TrackingId("XYZ"), STOCKHOLM, MELBOURNE);
-    registerEvent(cargoXYZ, "2007-11-30", HandlingEvent.Type.RECEIVE, null);
+  private Map<TrackingId, List<HandlingEvent>> eventMap = new HashMap<TrackingId, List<HandlingEvent>>();
 
-    final CarrierMovement stockholmToHamburg = voyageRepository.find(new CarrierMovementId("SESTO_DEHAM"));
-    registerEvent(cargoXYZ, "2007-12-01", HandlingEvent.Type.LOAD, stockholmToHamburg);
-    registerEvent(cargoXYZ, "2007-12-02", HandlingEvent.Type.UNLOAD, stockholmToHamburg);
-    
-    final CarrierMovement hamburgToHongKong = voyageRepository.find(new CarrierMovementId("DEHAM_CNHKG"));
-    registerEvent(cargoXYZ, "2007-12-03", HandlingEvent.Type.LOAD, hamburgToHongKong);
-    registerEvent(cargoXYZ, "2007-12-05", HandlingEvent.Type.UNLOAD, hamburgToHongKong);
-    
-    //CargoZYX
-    final Cargo cargoZYX = new Cargo(new TrackingId("ZYX"), MELBOURNE, STOCKHOLM);
-    registerEvent(cargoZYX, "2007-12-09", HandlingEvent.Type.RECEIVE, null);
-    
-    final CarrierMovement melbourneToTokyo = voyageRepository.find(new CarrierMovementId("AUMEL_JPTOK"));
-    registerEvent(cargoZYX, "2007-12-10", HandlingEvent.Type.LOAD, melbourneToTokyo);
-    registerEvent(cargoZYX, "2007-12-12", HandlingEvent.Type.UNLOAD, melbourneToTokyo);
-    
-    final CarrierMovement tokyoToLosAngeles = voyageRepository.find(new CarrierMovementId("JPTOK_USLA"));
-    registerEvent(cargoZYX, "2007-12-13", HandlingEvent.Type.LOAD, tokyoToLosAngeles);
- 
-    //CargoABC
-    final Cargo cargoABC = new Cargo(new TrackingId("ABC"), STOCKHOLM, HELSINKI);
-    registerEvent(cargoABC, "2008-01-01", HandlingEvent.Type.RECEIVE, null);
-    
-    final CarrierMovement stockholmToHelsinki = new CarrierMovement(
-            new CarrierMovementId("CAR_001"), STOCKHOLM, HELSINKI, new Date(), new Date());
-
-    registerEvent(cargoABC, "2008-01-02", HandlingEvent.Type.LOAD, stockholmToHelsinki);
-    registerEvent(cargoABC, "2008-01-03", HandlingEvent.Type.UNLOAD, stockholmToHelsinki);
-    registerEvent(cargoABC, "2008-01-05", HandlingEvent.Type.CLAIM, null);
-
-    //CargoCBA
-    final Cargo cargoCBA = new Cargo(new TrackingId("CBA"), HELSINKI, STOCKHOLM);
-    registerEvent(cargoCBA, "2008-01-10", HandlingEvent.Type.RECEIVE, null);
-    */
-  }
-
-  private void registerEvent(Cargo cargo, String date, HandlingEvent.Type type, CarrierMovement carrierMovement) throws ParseException {
-    /*
-    HandlingEvent event = new HandlingEvent(cargo, getCompletionTime(date), new Date(), type, null, carrierMovement);
-    String id = cargo.trackingId() + "_" + type + "_" + date;
-
-    logger.debug("Adding event " + id + "(" + event + ")");
-    eventDB.put(id, event);
-    */
-  }
-
-
+  @Override
   public void store(HandlingEvent event) {
-    eventDB.put(event.cargo().trackingId().idString(), event);
+    final TrackingId trackingId = event.cargo().trackingId();
+    List<HandlingEvent> list = eventMap.get(trackingId);
+    if (list == null) {
+      list = new ArrayList<HandlingEvent>();
+      eventMap.put(trackingId, list);
+    }
+    list.add(event);
   }
 
   @Override
   public HandlingHistory lookupHandlingHistoryOfCargo(TrackingId trackingId) {
-    return HandlingHistory.EMPTY;
-  }
-
-  /**
-   * Parse an ISO 8601 (YYYY-MM-DD) String to Date
-   *
-   * @param isoFormat String to parse.
-   * @return Created date instance.
-   * @throws ParseException Thrown if parsing fails.
-   */
-  private Date getDate(final String isoFormat) throws ParseException {
-    final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    return dateFormat.parse(isoFormat);
-  }
-
-  public void setCarrierRepository(final VoyageRepository voyageRepository) {
-    this.voyageRepository = voyageRepository;
+    List<HandlingEvent> events = eventMap.get(trackingId);
+    if (events == null) events = Collections.emptyList();
+    
+    return new HandlingHistory(events);
   }
 
 }
