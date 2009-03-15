59e3c2b8198cdeb9ec9de5604051e1efe373e633
peter_backlund <peterbacklund@users.noreply.github.com>
2009-03-15 14:11:33 +0000

    A few minor changes to the experimental parts of the domain layer support

--- dddsample/src/main/java/se/citerus/dddsample/domain/shared/experimental/Entity.java
+++ dddsample/src/main/java/se/citerus/dddsample/domain/shared/experimental/Entity.java
@@ -14,6 +14,11 @@ public interface Entity<T,ID> {
    */
   boolean sameIdentityAs(T other);
 
+  /**
+   * Entities have an identity.
+   *
+   * @return The identity of this entity.
+   */
   ID identity();
 
 }
