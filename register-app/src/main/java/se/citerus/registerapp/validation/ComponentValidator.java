54b2ec082b4ca43d858e6d021e2cb9384b273d1f
peter_backlund <peterbacklund@users.noreply.github.com>
2009-03-21 19:27:24 +0000

    Branch for 1.1.x development

--- register-app/src/main/java/se/citerus/registerapp/validation/ComponentValidator.java
+++
@@ -1,8 +0,0 @@
-package se.citerus.registerapp.validation;
-
-import javax.swing.JComponent;
-
-public interface ComponentValidator {
-  boolean validate(JComponent c);
-  String getMessage();
-}
--- register-app/startApp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -classpath .:target/register-app-1.0-SNAPSHOT-jar-with-dependencies.jar se.citerus.App
