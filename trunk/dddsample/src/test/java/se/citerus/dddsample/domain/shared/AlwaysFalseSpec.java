b0d208fd05809a1d200c032b6ed79b18cc3cce77
peter_backlund <peter_backlund@c75de98b-4f3f-0410-889e-e06553617c56>
2009-09-23 09:14:53 +0000

    Cleanup

--- trunk/dddsample/src/test/java/se/citerus/dddsample/domain/shared/AlwaysFalseSpec.java
+++
@@ -1,7 +0,0 @@
-package se.citerus.dddsample.domain.shared;
-
-public class AlwaysFalseSpec extends AbstractSpecification<Object> {
-  public boolean isSatisfiedBy(Object o) {
-    return false;
-  }
-}
--- trunk/dddsample/src/test/java/se/citerus/dddsample/domain/shared/AlwaysTrueSpec.java
+++
@@ -1,7 +0,0 @@
-package se.citerus.dddsample.domain.shared;
-
-public class AlwaysTrueSpec extends AbstractSpecification<Object> {
-  public boolean isSatisfiedBy(Object o) {
-    return true;
-  }
-}
