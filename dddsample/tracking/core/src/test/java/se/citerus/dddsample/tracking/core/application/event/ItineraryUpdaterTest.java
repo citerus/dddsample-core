dbadf52709f9ebdb92cccf3f2ef7d8a6a80c5b4f
peter_backlund <peterbacklund@users.noreply.github.com>
2009-10-15 14:56:14 +0000

    Added test for CargoUpdater

---
+++ dddsample/tracking/core/src/test/java/se/citerus/dddsample/tracking/core/application/event/ItineraryUpdaterTest.java
@@ -0,0 +1,13 @@
+package se.citerus.dddsample.tracking.core.application.event;
+
+import org.junit.Test;
+
+public class ItineraryUpdaterTest {
+
+  ItineraryUpdater itineraryUpdater;
+
+  @Test
+  public void something() {
+  }
+  
+}
