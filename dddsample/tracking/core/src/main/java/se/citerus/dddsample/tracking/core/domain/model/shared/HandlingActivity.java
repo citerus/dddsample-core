6d37b5944f710078bb8e908b3c49b36d331d1298
peter_backlund <peterbacklund@users.noreply.github.com>
2009-10-26 20:36:00 +0000

    Added DSL-like static factory methods for instance creation

--- dddsample/tracking/core/src/main/java/se/citerus/dddsample/tracking/core/domain/model/shared/HandlingActivity.java
+++ dddsample/tracking/core/src/main/java/se/citerus/dddsample/tracking/core/domain/model/shared/HandlingActivity.java
@@ -2,6 +2,7 @@ package se.citerus.dddsample.tracking.core.domain.model.shared;
 
 import org.apache.commons.lang.Validate;
 import se.citerus.dddsample.tracking.core.domain.model.handling.HandlingEvent;
+import static se.citerus.dddsample.tracking.core.domain.model.handling.HandlingEvent.Type.*;
 import se.citerus.dddsample.tracking.core.domain.model.location.Location;
 import se.citerus.dddsample.tracking.core.domain.model.voyage.Voyage;
 import se.citerus.dddsample.tracking.core.domain.patterns.valueobject.ValueObjectSupport;
@@ -12,10 +13,6 @@ import se.citerus.dddsample.tracking.core.domain.patterns.valueobject.ValueObjec
  * happen to a cargo in the future.
  */
 public class HandlingActivity extends ValueObjectSupport<HandlingActivity> {
-
-  // TODO introduce something like this (?):
-  // HandlingActivity.loadOnto(voyage).in(location)
-  // HandlingActivity.claimIn(location)
 
   private final HandlingEvent.Type type;
   private final Location location;
@@ -62,6 +59,42 @@ public class HandlingActivity extends ValueObjectSupport<HandlingActivity> {
     type = null;
     location = null;
     voyage = null;
+  }
+
+  // DSL-like factory methods
+
+  public static InLocation loadedOnto(Voyage voyage) {
+    return new InLocation(LOAD, voyage);
+  }
+
+  public static InLocation unloadedOff(Voyage voyage) {
+    return new InLocation(UNLOAD, voyage);
+  }
+
+  public static HandlingActivity receivedIn(Location location) {
+    return new HandlingActivity(RECEIVE, location);
+  }
+
+  public static HandlingActivity claimedIn(Location location) {
+    return new HandlingActivity(CLAIM, location);
+  }
+
+  public static HandlingActivity customsIn(Location location) {
+    return new HandlingActivity(CUSTOMS, location);
+  }
+
+  public static class InLocation {
+    private final HandlingEvent.Type type;
+    private final Voyage voyage;
+
+    public InLocation(HandlingEvent.Type type, Voyage voyage) {
+      this.type = type;
+      this.voyage = voyage;
+    }
+
+    public HandlingActivity in(Location location) {
+      return new HandlingActivity(type, location, voyage);
+    }
   }
 
 }
