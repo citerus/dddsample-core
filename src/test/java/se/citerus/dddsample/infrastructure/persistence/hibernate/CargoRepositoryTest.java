127aaf83c8f9aa1a7397133ff5a0733368d811c4
marlin <markus.christoffer.lindgren@gmail.com>
2015-09-21 20:49:18 +0200

    Bump Spring to 4.2.1.

--- src/test/java/se/citerus/dddsample/infrastructure/persistence/hibernate/CargoRepositoryTest.java
+++ src/test/java/se/citerus/dddsample/infrastructure/persistence/hibernate/CargoRepositoryTest.java
@@ -177,7 +177,7 @@ public class CargoRepositoryTest {
     public void testReplaceItinerary() {
         Cargo cargo = cargoRepository.find(new TrackingId("FGH"));
         Long cargoId = getLongId(cargo);
-        assertEquals(3, jdbcTemplate.queryForInt("select count(*) from Leg where cargo_id = ?", cargoId));
+        assertEquals(3, numberOfLegs(cargoId));
 
         Location legFrom = locationRepository.find(new UnLocode("FIHEL"));
         Location legTo = locationRepository.find(new UnLocode("DEHAM"));
@@ -188,7 +188,11 @@ public class CargoRepositoryTest {
         cargoRepository.store(cargo);
         flush();
 
-        assertEquals(1, jdbcTemplate.queryForInt("select count(*) from Leg where cargo_id = ?", cargoId));
+        assertEquals(1, numberOfLegs(cargoId));
+    }
+
+    private int numberOfLegs(long cargoId) {
+        return jdbcTemplate.queryForObject("select count(*) from Leg where cargo_id = ?", new Long[]{cargoId}, Integer.class);
     }
 
     @Test
