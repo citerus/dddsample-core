078a7ae68c33971788d2706eeb1fcd000c323a8c
Andy Marks <amarks@thoughtworks.com>
2017-06-01 10:45:21 +1000

    Starting to assert deeper into customer page

--- src/test/java/se/citerus/dddsample/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/se/citerus/dddsample/acceptance/CustomerAcceptanceTest.java
@@ -1,6 +1,7 @@
 package se.citerus.dddsample.acceptance;
 
 import org.junit.Test;
+import se.citerus.dddsample.acceptance.pages.CustomerPage;
 import se.citerus.dddsample.acceptance.pages.LaunchPage;
 
 public class CustomerAcceptanceTest extends AbstractAcceptanceTest {
@@ -8,7 +9,11 @@ public class CustomerAcceptanceTest extends AbstractAcceptanceTest {
     public void customerSiteIsOperational() {
         LaunchPage home = new LaunchPage(driver,"http://localhost:8080/");
 
-        home.goToCustomerPage();
+        CustomerPage customerPage = home.goToCustomerPage();
+        customerPage.trackCargoWithIdOf("ABC123");
+        customerPage.expectCargoLocation("New York");
+
+
     }
 
 }
--- src/test/java/se/citerus/dddsample/acceptance/pages/CustomerPage.java
+++ src/test/java/se/citerus/dddsample/acceptance/pages/CustomerPage.java
@@ -1,11 +1,29 @@
 package se.citerus.dddsample.acceptance.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class CustomerPage {
+    private final WebDriver driver;
+
     public CustomerPage(WebDriver driver) {
+        this.driver = driver;
         assertEquals("Tracking cargo", driver.getTitle());
+    }
+
+    public void trackCargoWithIdOf(String trackingId) {
+        WebElement element = driver.findElement(By.id("idInput"));
+        element.sendKeys(trackingId);
+        element.submit();
+
+    }
+
+    public void expectCargoLocation(String expectedLocation) {
+        WebElement cargoSummary = driver.findElement(By.cssSelector("#result h2"));
+        assertTrue(cargoSummary.getText().endsWith(expectedLocation));
     }
 }
