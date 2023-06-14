bd4dda2c57f38567aa39dc2754a52a9f1aaa9c71
Johan Rende <johan@rende.se>
2023-06-14 15:38:01 +0200

    Revert "Removed unused class"
    This reverts commit bfce1d3b29e2a2820db97864ae10a2d15e52f577.

---
+++ src/main/java/se/citerus/dddsample/interfaces/booking/web/RouteAssignmentCommand.java
@@ -0,0 +1,75 @@
+package se.citerus.dddsample.interfaces.booking.web;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RouteAssignmentCommand {
+
+  private String trackingId;
+  private List<LegCommand> legs = new ArrayList<>();
+
+  public String getTrackingId() {
+    return trackingId;
+  }
+
+  public void setTrackingId(String trackingId) {
+    this.trackingId = trackingId;
+  }
+
+  public List<LegCommand> getLegs() {
+    return legs;
+  }
+
+  public void setLegs(List<LegCommand> legs) {
+    this.legs = legs;
+  }
+
+  public static final class LegCommand {
+    private String voyageNumber;
+    private String fromUnLocode;
+    private String toUnLocode;
+    private Instant fromDate;
+    private Instant toDate;
+
+    public String getVoyageNumber() {
+      return voyageNumber;
+    }
+
+    public void setVoyageNumber(final String voyageNumber) {
+      this.voyageNumber = voyageNumber;
+    }
+
+    public String getFromUnLocode() {
+      return fromUnLocode;
+    }
+
+    public void setFromUnLocode(final String fromUnLocode) {
+      this.fromUnLocode = fromUnLocode;
+    }
+
+    public String getToUnLocode() {
+      return toUnLocode;
+    }
+
+    public void setToUnLocode(final String toUnLocode) {
+      this.toUnLocode = toUnLocode;
+    }
+
+    public Instant getFromDate() {
+      return fromDate;
+    }
+
+    public void setFromDate(Instant fromDate) {
+      this.fromDate = fromDate;
+    }
+
+    public Instant getToDate() {
+      return toDate;
+    }
+
+    public void setToDate(Instant toDate) {
+      this.toDate = toDate;
+    }
+  }
+}
